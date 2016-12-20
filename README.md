05c84bb7ede32ae8bba1924cc01bbaca8ed88375
Liichy Aguilar Bueno <Liichy Aguilar Bueno>
2016-12-20 11:29:15 -0500

    sueldo semanal

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Ejercicio3
+    Inicio: SueldoSemanal
+
+##  Ingreso datos
+
+- horas
+- pago
+- resultado
+
+## Proceso
+- pedir horas
+- pedir pago
+- Multiplicar horas con pago
+- mostrar el resultado
+
+*fin*#Ejercicio3
---
+++ main.js
@@ -0,0 +1,10 @@
+var horas, pago, SueldoSemanal;
+
+horas = prompt ("Por favor ingresa un número");
+pago = prompt ("Por favor ingresa otro número");
+
+
+SueldoSemanal = parseInt (horas) * parseInt (pago);
+
+
+alert(SueldoSemanal);
